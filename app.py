9ca2db796b2d6e7d5e9e9e33bbc59ee3e993ece6
CyberCloudUK <118209559+CyberCloudUK@users.noreply.github.com>
2022-11-14 19:03:13 +0000

    input

---
+++ app.py
@@ -0,0 +1,22 @@
+print('Hello World')
+
+age = 20
+price = 19.95
+first_name = 'CyberCloudUK.com'
+is_online = True
+print(age, price, first_name)
+
+
+patient = 'John Smith'
+age = 20
+patient_type = 'New'
+
+print(
+   patient,
+   age,
+   patient_type
+)
+
+
+name = input('What is your name? ')
+print ('Hello ' +name)

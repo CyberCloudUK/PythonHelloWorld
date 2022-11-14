1187ee78d24a1ed518efbd9876fdc944ec5cc395
CyberCloudUK <118209559+CyberCloudUK@users.noreply.github.com>
2022-11-14 19:11:46 +0000

    calculate birth year - 2022

--- app.py
+++ app.py
@@ -1,22 +1,3 @@
-print('Hello World')
-
-age = 20
-price = 19.95
-first_name = 'CyberCloudUK.com'
-is_online = True
-print(age, price, first_name)
-
-
-patient = 'John Smith'
-age = 20
-patient_type = 'New'
-
-print(
-   patient,
-   age,
-   patient_type
-)
-
-
-name = input('What is your name? ')
-print ('Hello ' +name)
+birth_year = input('Enter your year of birth: ')
+age = 2022 - int(birth_year)
+print(age)
